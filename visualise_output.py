f0a3fc5562a0fd5358d470e9fc89d637407f80b3
ahua352 <ahua352@aucklanduni.ac.nz>
2024-07-25 11:12:14 +1200

    Display npy output data

---
+++ visualise_output.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Main function
+def main():
+    filename = 'outputs/gJB_sBM_cAll_d09_mJB5_ch01_mJB5.npy' 
+    data = np.load(filename)
+    print(data)
+    print(data.shape)
+
+    np.savetxt('data.txt', data, fmt='%s')
+
+if __name__ == '__main__':
+    main()
