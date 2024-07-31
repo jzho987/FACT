9e72c93872686d3e6fa0bafc54693ccb0d589966
ahua352 <ahua352@aucklanduni.ac.nz>
2024-07-31 17:56:03 +1200

    Added utils for converting npy to pkl and loading pkl
    Inspired from these: https://github.com/google-research/mint/issues/39
    https://github.com/caijianfei/google-dancing/blob/main/exporter.py

---
+++ utils/pkl_converter.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+import pickle
+
+# Main function
+def main():
+    # Used loss and scaling from gBR_sBM_cAll_d04_mBR0_ch01
+    my_dict = {
+        "smpl_loss": 1.5907235145568848,
+        "smpl_poses": None,
+        "smpl_scaling": np.array([93.77886], dtype=np.float32),
+        "smpl_trans": None,
+    }
+
+    filename = 'outputs_2/gPO_sBM_cAll_d10_mPO1_ch02_mHO5.npy' 
+    data = np.load(filename)
+    data = np.array(data) # (N, 225)
+
+    trans = data[:, 6:9]
+    poses = data[:, 9:]
+
+    poses = R.from_matrix(poses.reshape(-1, 3, 3)).as_rotvec().reshape(-1, 72)
+
+    my_dict["smpl_poses"] = poses
+    my_dict["smpl_trans"] = trans
+
+    print("my_dict")
+    print(my_dict)
+
+    dict_filename = 'output.pkl'
+    with open(dict_filename, 'wb') as f:
+        pickle.dump(my_dict, f)
+
+if __name__ == '__main__':
+    main()
---
+++ utils/pkl_loader.py
@@ -0,0 +1,14 @@
+import pickle
+import numpy as np;
+
+# Main function
+def main():
+    filename = 'data/aist_plusplus_final/motions/gBR_sBM_cAll_d04_mBR0_ch01.pkl'
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+
+    print("data")
+    print(data)
+
+if __name__ == '__main__':
+    main()
